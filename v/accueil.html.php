5585d837384f9149622bd1cd37472bd0256eca98
michaeljpitz <michaeljpitz@gmail.com>
2018-03-23 11:27:08 +0100

    jquery

--- v/accueil.html.php
+++ v/accueil.html.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Notre site - Accueil</title>
+    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css"/>
+    <script src="public/jquery.min.js"></script>
+    <script src="public/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 <h1>Notre site - Accueil</h1>
