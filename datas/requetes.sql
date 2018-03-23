9c0f59847716b6f23bae9d363ed47c6a16a412fd
michaeljpitz <michaeljpitz@gmail.com>
2018-03-23 09:53:27 +0100

    select + image.png db

--- datas/requetes.sql
+++ datas/requetes.sql
@@ -6,3 +6,19 @@ SELECT * FROM user;
 SELECT * FROM categ;
 # sélection des permission
 SELECT * FROM permission;
+
+# sélection du 'title' et 'content' des 'news' et le 'name' venant de la table 'user'
+SELECT n.title, n.content, u.name
+	FROM news n
+    INNER JOIN user u 
+    ON n.user_iduser = u.iduser;
+	
+
+# sélection du 'title' et 'content' des 'news' et le 'name' venant de la table 'user' ainsi que le 'name' renommé en 'pname' et le 'level' de la table permission
+SELECT n.title, n.content, u.name, p.name AS pname, p.level
+	FROM news n
+    INNER JOIN user u 
+		ON n.user_iduser = u.iduser
+    INNER JOIN permission p 
+		on u.permission_idpermission = p.idpermission
+    ;
