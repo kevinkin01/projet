8b612d54afdea1f961c2288b75bdbccdb3a42190
Kevin Kinanga <kevin.kinanga@cf2m.lan>
2018-03-28 15:27:49 +0200

    version 2

---
+++ c/CVController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin.kinanga
+ * Date: 27/03/2018
+ * Time: 14:48
+ */
---
+++ v/contact.html.php
@@ -0,0 +1,14 @@
+<?php include_once "header.html.php"?>
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+include "menu.html.php"
+?>
